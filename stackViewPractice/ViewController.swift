8588587e94e3a01cd3ffb90e9a8313840ac699b8
bhtran <bhtran00@gmail.com>
2016-07-20 15:33:56 -0400

    initial commmit

---
+++ stackViewPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackViewPractice
+//
+//  Created by Bao Tran on 7/20/16.
+//  Copyright © 2016 Bao Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
